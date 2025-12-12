346a0a1e4e1a5d1356fddf31bab297d57d85a940
Muqeetat <aderintotolulope@gmail.com>
2025-12-12 08:18:05 +0100

    Initial commit

---
+++ EmployeeManagement/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        // Navigation
+        public List<Employee> Employees { get; set; } = [];
+    }
+
+}
